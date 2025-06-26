acfeadd226b71ebd3bef36a4a21275fdfab05d9f
Naoki Watanabe <asterisk37n@gmail.com>
2025-06-26 22:10:47 +0900

    define ids in constants file

--- src/constants.ts
+++ src/constants.ts
@@ -8,3 +8,7 @@ export const OPEN_LINK_ID = 'openLink';
 export const SWITCH_TO_IMAGE_SEARCH_ID = 'switchToImageSearch';
 export const SWITCH_TO_ALL_SEARCH_ID = 'switchToAllSearch';
 export const SWITCH_TO_YOUTUBE_ID = 'switchToYouTube';
+export const SWITCH_TO_VIDEOS_ID = 'switchToVideos';
+export const SWITCH_TO_SHOPPING_ID = 'switchToShopping';
+export const SWITCH_TO_NEWS_ID = 'switchToNews';
+export const SWITCH_TO_MAP_ID = 'switchToMap';
